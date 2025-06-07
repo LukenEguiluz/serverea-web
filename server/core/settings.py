2b51556d760b8e6c8fc44d4301404ca417793c3a
Luken Eguiluz <luken.eguiluz@gmail.com>
2025-06-07 00:13:20 -0600

    Agregados dockerfiles

--- server/core/settings.py
+++ server/core/settings.py
@@ -87,7 +87,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',  # ./server/db/db.sqlite3
+
     }
 }
 
